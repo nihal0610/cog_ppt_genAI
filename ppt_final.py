67d36e2a25efb5a962107a13d59250ee04b812e8
nihal0610 <168910699+nihal0610@users.noreply.github.com>
2025-02-03 12:43:34 +0530

    Update ppt_final.py

--- ppt_final.py
+++ ppt_final.py
@@ -46,7 +46,7 @@ def main():
             
             # Initialize the regular model
             llm = LLM(
-                model="gpt-3.5-turbo-16k", temperature=0.3, api_key=openai_api_key
+                model="gpt-4o", temperature=0.3, api_key=openai_api_key
             )
             search_tool = SerperDevTool(serpapi_key=serpapi_key)
 
